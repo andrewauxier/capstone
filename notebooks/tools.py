226bb351c115e5fcd9e0c49e2b32695fb0b8e6bb
Auxier <1367073752121002@mil>
2025-08-04 16:02:13 -0400

    Cleaned up the code, added functions to a tools file

---
+++ notebooks/tools.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def eda(df: pd.DataFrame):
+    """
+    Run basic EDA on a dataframe.
+
+    Args:
+        Takes the desired dataframe as an argument.
+    
+    Returns:
+        The column names, shape, data types, statistical description, and null values of the dataframe.
+    """
+    print('Columns:', df.columns)
+    print('Shape:', df.shape)
+    print('Data Types:', df.dtypes)
+    print('Describe:', df.describe())
+    print('Null Values:', df.isnull().sum())
+
+### Function for handling null values in a dataframe ###
+def null(df:pd.DataFrame):
+
+    """
+    Removes the null values from a specified dataframe
+    
+    Args:
+        Takes the desired dataframe as a argument
+    
+    Returns:
+        Drops the null values and prints a summation of the null values in a dataframe (which should be 0).
+    """
+    df = df.dropna()
+    print('Nulls are gone!', df.isnull().sum())
+
+
